c9c50d1f52d570db2cd94867601ca316ff625253
yufeng-tan <51103555+yufeng-tan@users.noreply.github.com>
2019-05-28 21:49:10 -0400

    TASK 1
    (HL) Define data structures to represent J0 programs.

---
+++ TASK_1.py
@@ -0,0 +1,17 @@
+# TASK 1
+# Define data structures to represent J0 programs. 
+
+class Add:
+    def __init__(self, l, r):
+        self.l = l
+        self.r = r
+
+class Num:
+    def __init__(self, n):
+        self.n = n
+
+class Mult:
+    def __init__(self, l, r):
+        self.l = l
+        self.r = r
+
