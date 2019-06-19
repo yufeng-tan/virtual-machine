eb23d33843c63063394e5bae5d70316fc2f4f259
yufeng-tan <51103555+yufeng-tan@users.noreply.github.com>
2019-06-19 12:51:50 -0400

    Update TASK_12.py

--- TASK_12.py
+++ TASK_12.py
@@ -3,4 +3,20 @@
 
 class C:
     def __init__(self, list):
-        self.list = list
+        if list[0] == 'HOLE' or list[1] == 'HOLE' or list[2] == 'HOLE':
+            self.list = list
+        else:
+            self.list = ['error']
+
+    def interp(self):
+        return self.list
+
+    def __str__(self):
+        return str(self.interp())
+
+
+
+print(C(['e', 'e', 'e']))
+
+
+print(C(['e', 'HOLE', 'e']))
