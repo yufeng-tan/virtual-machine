9ebd771b63691bea0c70cc031c6fff515aea2654
yufeng-tan <51103555+yufeng-tan@users.noreply.github.com>
2019-06-20 22:41:21 -0400

    Update TASK_15.py

--- TASK_15.py
+++ TASK_15.py
@@ -4,3 +4,49 @@
 test_string = '(if (< (+ 1 1) 5) 3 4)'
 # return (if (< HOLE 5) 3 4) and (+ 1 1)
 # return (if HOLE 3 4 ) and (< HOLE 5) and (+ 1 1)
+
+
+# function takes a string and returns the maximum depth nested parenthsis
+def maxDepth(s):
+    current_max = 0
+    max = 0
+    n = len(s)
+
+    for i in range(n):
+        if s[i] == '(':
+            current_max += 1
+            if current_max > max:
+                max = current_max
+        elif s[i] == ')':
+            if current_max > 0:
+                current_max -= 1
+            else:
+                return -1
+
+    if current_max != 0:
+        return -1
+
+    return max
+
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
+
+
+# error: RecursionError: maximum recursion depth exceeded in comparison
+# I will figure it out later
+def find_redex(s):
+    max_parenthsis = maxDepth(s)
+    max_parenthsis_start = find_nth(test_string, '(', maxDepth(test_string))
+    max_parenthsis_end = find_nth(test_string, ')', 1)
+    redex = s[max_parenthsis_start:max_parenthsis_end+1]
+    new_s = find_redex(s.replace(redex, 'HOLE'))
+    ss = find_redex(new_s) + ' ' + redex
+    return ss
+    
+
+print(find_redex(test_string))
+# (if (< HOLE 5) 3 4) (+ 1 1)
