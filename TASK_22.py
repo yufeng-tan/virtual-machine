06f3f1423ff865273adde971ba66dbb7ed7c4ad4
yufeng-tan <51103555+yufeng-tan@users.noreply.github.com>
2019-06-30 16:19:33 -0400

    TASK 22
    Implement the CK0 machine interpreter for J1.

---
+++ TASK_22.py
@@ -0,0 +1,24 @@
+# Implement the CK0 machine interpreter for J1. 
+
+test_1 = 'if 2<5 true then 2 else 5 k0'
+# ['if', '2<5', 'true', 'then', '2', 'else', '5', 'k0']
+test_2 = 'true kif 2 5 k0'
+
+
+def convert(string):
+    li = list(string.split(' '))
+    return li
+
+
+def CK0_interp(expr):
+    li = convert(expr)
+
+    if li[0] == 'if':
+        lt = li[1] + ' ' + 'kif' + ' ' + li[4] + ' ' +  li[6] + ' ' + li[7]
+        return convert(lt)
+    if li[0] == 'true':
+        lt = li[2] + ' ' + 'k0'
+        return convert(lt)
+
+print(CK0_interp(test_1))
+print(CK0_interp(test_2))
