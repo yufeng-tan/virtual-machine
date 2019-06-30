af1c72e5b0419ba898f3cde2fb68bfed6a467a3c
yufeng-tan <51103555+yufeng-tan@users.noreply.github.com>
2019-06-29 23:09:20 -0400

    TASK 25
    (HL) Write a test-suite of a dozen J2 programs.

---
+++ TASK_25.py
@@ -0,0 +1,13 @@
+# (HL) Write a test-suite of a dozen J2 programs. 
+
+test_1 = ['g', '10']
+
+test_2 = ['f', '9', '1']
+
+test_3 = ['f', '9', ['g', '1']]
+
+test_4 = ['+', ['g', '10'], ['f', '9', ['g', '1']]]
+
+
+
+
