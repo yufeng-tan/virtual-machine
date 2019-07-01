d49be39269ba2f50e10c82ac0d96170badfd2424
yufeng-tan <51103555+yufeng-tan@users.noreply.github.com>
2019-07-01 11:26:06 -0400

    Update TASK_31.py

--- TASK_31.py
+++ TASK_31.py
@@ -18,8 +18,15 @@ test_5 = ['delta', 'true', 'env', 'kif', '2', '5', 'k']
 # return ['delta', '5', 'env', 'k']
 test_6 = ['delta', 'false', 'env', 'kif', '2', '5', 'k']
 
+# return ['delta', 'e0', 'env', 'kapp', 'v0...', 'e1...', 'k']
+test_7 = ['delta', 'vn', 'env', 'kapp', 'v0...', 'e0...', 'k']
+
+# return ['delta', 'delta(p, v0...vn)', 'env', 'k']
+test_8 = ['delta', 'vn', 'env', 'kapp', 'pv0', 'v0', ' ', 'k']
+
+
 # return 'Let define (f x0...) e) = delta f' + ['delta', 'e', 'env[x0->v0]', 'k']
-test_7 = ['delta', 'vn', 'env', 'kapp', 'fvo', ' ', 'k' ]
+test_9 = ['delta', 'vn', 'env', 'kapp', 'fv0', ' ', 'k' ]
 
 def CK1(expr):
     if expr[1] == 'x' and expr[2] == 'mt':
@@ -37,6 +44,10 @@ def CK1(expr):
         return ['delta', expr[4], 'env', 'k']
     if expr[1] == 'false':
         return ['delta', expr[5], 'env', 'k']
+    if expr[4] == 'v0...':
+        return ['delta', 'e0', expr[2], expr[3], expr[4], 'e1...', expr[6]]
+    if expr[1] == 'vn' and expr[4] == 'pv0':
+        return ['delta', 'delta(p, v0...vn)', expr[2], expr[7]]
     if expr[1] == 'vn':
         def_str = 'Let define (f x0...) e) = delta f'
         li = ['delta', 'e', 'env[x0->v0]', 'k']
@@ -58,3 +69,7 @@ print(CK1(test_5))
 print(CK1(test_6))
 
 print(CK1(test_7))
+
+print(CK1(test_8))
+
+print(CK1(test_9))
