7412e016efebf6468bae755579d9eadfca5920be
yufeng-tan <51103555+yufeng-tan@users.noreply.github.com>
2019-07-02 04:58:34 -0400

    TASK 39
    Extend your J3 data structures to J4.

---
+++ TASK_39.py
@@ -0,0 +1,31 @@
+# Extend your J3 data structures to J4. 
+
+# e::= x | v | (e e ...) | (if e e e )
+# v::= number | boolean | prim | lambda x (x ...) e
+# x::= variable-not-otherwise-mentioned
+# prim::= + | * | / | - | <= | < | = | > | >=
+
+prim_list = ['+', '*', '/', '-', '<=', '<', '=', '>', '>=']
+
+class x:
+    def __init__(self, x):
+        self.x = x
+    def __str__(self):
+        return str(self.x)
+
+class V:
+    def __init__(self, v):
+        if isinstance(v, (int, bool,f)) or str(v) in prim_list or isinstance(v, str):
+            self.v = v
+        else:
+            self.v = 'error'
+
+    def interp(self):
+        return self.v
+
+    def __str__(self):
+        return str(self.interp())
+
+class E:
+    def __init__(self, list):
+        self.list = list
